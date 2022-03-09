cab2526299ebbc933029651497103701c3b08398
VietDuc Pham <phamvietducfpt@gmail.com>
2022-03-09 16:35:57 +0700

    update .giginote again

--- src/App.js
+++ src/App.js
@@ -19,29 +19,29 @@ function App() {
     //   </header>
     // </div>
     <div className="background-image">
-    <div className="loadding">
+      <div className="loadding">
         <h3 className="loadding-text"><span>Redirect to homepage after </span> <span className="time-load">3</span><span className="seconds">s</span></h3>
-    </div>
-    <div className="box-info">
-    <div className="box-info-inner">
-        <div className="hoverBtn"></div>
-        <div className="hoverBtn-bottom"></div>
+      </div>
+      <div className="box-info">
+        <div className="box-info-inner">
+          <div className="hoverBtn"></div>
+          <div className="hoverBtn-bottom"></div>
 
-        <div className="wellcome">
+          <div className="wellcome">
             <h2 className="text-wellcome"><p>Welcomes to</p></h2>
-        </div>
-        <div className="main-title">
+          </div>
+          <div className="main-title">
             <h1 className="text-main-title"><span>A "autistic" place of my own</span></h1>
-        </div>
-        <div className="des-title">
+          </div>
+          <div className="des-title">
             <a className="destitle-text"><span>Make you know me better and come out with me</span></a>
-        </div>
-        <div className="link-website">
+          </div>
+          <div className="link-website">
             <a className="link-text"><span>VIETDUCBLOG.ME</span></a>
+          </div>
         </div>
-    </div>  
+      </div>
     </div>
-</div>
   );
 }
 
