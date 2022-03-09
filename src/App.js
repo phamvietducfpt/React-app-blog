5e6be42a843ccb26f8780043e8aaf4f24a1765d2
VietDuc Pham <phamvietducfpt@gmail.com>
2022-03-09 16:28:56 +0700

    update .giginote add bootstrap and change index app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './assets/images/logo.svg';
-import '.s/styles/App.css';
-
+import './styles/App.scss';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Hello World with Reactjs</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div classNameName="App">
+    //   <header classNameName="App-header">
+    //     <img src={logo} classNameName="App-logo" alt="logo" />
+    //     <p>
+    //       <h1>Hello World with Reactjs</h1>
+    //     </p>
+    //     <a
+    //       classNameName="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div className="background-image">
+    <div className="loadding">
+        <h3 className="loadding-text"><span>Redirect to homepage after </span> <span className="time-load">3</span><span className="seconds">s</span></h3>
     </div>
+    <div className="box-info">
+    <div className="box-info-inner">
+        <div className="hoverBtn"></div>
+        <div className="hoverBtn-bottom"></div>
+
+        <div className="wellcome">
+            <h2 className="text-wellcome"><p>Welcomes to</p></h2>
+        </div>
+        <div className="main-title">
+            <h1 className="text-main-title"><span>A "autistic" place of my own</span></h1>
+        </div>
+        <div className="des-title">
+            <a className="destitle-text"><span>Make you know me better and come out with me</span></a>
+        </div>
+        <div className="link-website">
+            <a className="link-text"><span>VIETDUCBLOG.ME</span></a>
+        </div>
+    </div>  
+    </div>
+</div>
   );
 }
 
